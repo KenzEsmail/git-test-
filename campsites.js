00569d81274d3c0d91330ddd4b4432c91d6455e2
KenzEsmail <81309969+KenzEsmail@users.noreply.github.com>
2021-06-01 13:05:35 -0400

    Add files via upload

---
+++ campsites.js
@@ -0,0 +1,30 @@
+export const CAMPSITES = [
+    {
+        id: 0,
+        name: 'Women World',
+        image: 'assets/images/women.jpeg',
+        elevation: 1233,
+        featured: false,
+        description: "Spoil her with the nice, comfortable, colorful  and modest dresses.",    },
+    {
+        id: 1,
+        name: 'Men World',
+         image: 'assets/images/men.jpeg',
+        elevation: 877,
+        featured: false,
+        description: "Different culture clothing from the men world.",    },
+    {
+        id: 2,
+        name: 'Children World',
+        image: 'assets/images/kids1.jpeg',
+        elevation: 2901,
+        featured: false,
+        description: "Show your love and care for your child with children world.",    },
+    {
+        id: 3,
+        name: 'Purses World',
+        image: 'assets/images/purses.jpg',
+        elevation: 42,
+        featured: true,
+        description: "love purses? shop here for your purses world.",    }
+];
